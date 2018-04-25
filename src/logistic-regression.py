83dea2f2d209a7609013797291bf39b3a8e8f13c
SriKrishna Sadula <krishna.sadula@gmail.com>
2018-04-25 18:32:06 +0530

    python logistic regression updated

--- src/logistic-regression.py
+++ src/logistic-regression.py
@@ -4,9 +4,7 @@ Created on Mon Apr 23 09:37:50 2018
 
 @author: Srikrishna.Sadula
 """
-
 # banking services data for logistic regression
-
 import pandas as pd
 import numpy as np
 from sklearn import preprocessing
@@ -17,7 +15,6 @@ from sklearn.cross_validation import train_test_split
 import seaborn as sns
 sns.set(style="white")
 sns.set(style="whitegrid", color_codes=True)
-
 
 #preparing data
 data = pd.read_csv("..//data//banking.csv", header=0)
@@ -46,3 +43,43 @@ plt.title('Purchase Frequency for Job Title')
 plt.xlabel('Job')
 plt.ylabel('Frequency of Purchase')
 plt.savefig('purchase_fre_job')
+
+#create dummy variable aka encoding
+cat_vars=['job','marital','education','default','housing','loan','contact','month','day_of_week','poutcome']
+for var in cat_vars:
+    cat_list='var'+'_'+var
+    cat_list = pd.get_dummies(data[var], prefix=var)
+    data1=data.join(cat_list)
+    data=data1
+cat_vars=['job','marital','education','default','housing','loan','contact','month','day_of_week','poutcome']
+
+data_vars=data.columns.values.tolist()
+to_keep=[i for i in data_vars if i not in cat_vars]
+
+data_final=data[to_keep]
+data_final.columns.values
+
+data_final_vars=data_final.columns.values.tolist()
+y=['y']
+X=[i for i in data_final_vars if i not in y]
+
+#Recursive Feature elimination ***
+from sklearn import datasets
+from sklearn.feature_selection import RFE
+from sklearn.linear_model import LogisticRegression
+logreg = LogisticRegression()
+rfe = RFE(logreg, 18)
+rfe = rfe.fit(data_final[X], data_final[y])
+print(rfe.support_)
+print(rfe.ranking_)
+
+cols=["previous", "euribor3m", "job_blue-collar", "job_retired", "job_services", "job_student", "default_no", 
+      "month_aug", "month_dec", "month_jul", "month_nov", "month_oct", "month_sep", "day_of_week_fri", "day_of_week_wed", 
+      "poutcome_failure", "poutcome_nonexistent", "poutcome_success"] 
+X=data_final[cols]
+y=data_final['y']
+
+import statsmodels.api as sm
+logit_model=sm.Logit(y,X)
+result=logit_model.fit()
+print(result.summary())
