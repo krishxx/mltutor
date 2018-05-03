3e93571d267715ead9f66bc5af3f57a689936028
SriKrishna Sadula <krishna.sadula@gmail.com>
2018-05-03 06:39:02 +0530

    neuralnetwork.py basic neural network example added

---
+++ src/neuralnetwork.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 30 17:21:36 2018
+
+@author: Srikrishna.Sadula
+"""
+
+import numpy as np
+
+#input array
+X = np.array([[1, 0, 1, 0],
+             [1, 0, 1, 1],
+             [0, 1, 0, 1]])
+
+#output array
+y = np.array([[1],
+              [1],
+              [0]])
+
+#Sigmoid function
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+#Derivative of sigmoid function
+def derivatives_sigmoid(x):
+    return x*(1-x)
+
+#variable initialilzation
+epoch = 5000 #Setting training iterations
+lr = 0.1 #setting learning rate
+inputlayer_neurons = X.shape[1] #number of features in a dataset
+hiddenlayer_neurons = 3
+output_neurons = 1
+
+#weight and bias initialization
+wh = np.random.uniform(size=(inputlayer_neurons, hiddenlayer_neurons))
+bh = np.random.uniform(size=(1, hiddenlayer_neurons))
+wout = np.random.uniform(size=(hiddenlayer_neurons, output_neurons))
+bout=np.random.uniform(size=(1,output_neurons))
+
+for i in range(epoch):
+    #Forward Propogation
+    hidden_layer_input1=np.dot(X,wh)
+    hidden_layer_input=hidden_layer_input1 + bh
+    hiddenlayer_activations = sigmoid(hidden_layer_input)
+    output_layer_input1=np.dot(hiddenlayer_activations,wout)
+    output_layer_input= output_layer_input1+ bout
+    output = sigmoid(output_layer_input)
+
+    #Backpropagation
+    E = y-output
+    slope_output_layer = derivatives_sigmoid(output)
+    slope_hidden_layer = derivatives_sigmoid(hiddenlayer_activations)
+    d_output = E * slope_output_layer
+    Error_at_hidden_layer = d_output.dot(wout.T)
+    d_hiddenlayer = Error_at_hidden_layer * slope_hidden_layer
+    wout += hiddenlayer_activations.T.dot(d_output) *lr
+    bout += np.sum(d_output, axis=0,keepdims=True) *lr
+    wh += X.T.dot(d_hiddenlayer) *lr
+    bh += np.sum(d_hiddenlayer, axis=0,keepdims=True) *lr
+
+print(output)
